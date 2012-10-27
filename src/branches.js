cf17e43b219b9a184f7390733c6f6e125803f54e
fengb <benjamin.feng@gmail.com>
2012-10-26 23:13:16 -0500

    Refactored to use nicer API

--- src/branches.js
+++ src/branches.js
@@ -1,35 +1,41 @@
-var Branches = {
-  next: function(input) {
-    var output = [];
-    for(var i=0; i < input.length + 1; i++) {
-      output[i] = ' ';
-    }
+function Branches(val) {
+  this.val = (typeof val == 'string') ? val.split('') : val;
+}
 
-    for(i=0; i < input.length; i++) {
-      switch(input[i]) {
-        case '<':
-          output[i] = output[i] == ' ' ? '<' : ' ';
-          break;
-        case '>':
-          output[i + 1] = '>';
-          break;
-        case 'X':
-          output[i] = output[i] == ' ' ? '<' : ' ';
-          output[i + 1] = '>';
-          break;
-      }
-    }
+Branches.prototype.toString = function() {
+  return this.val.join('');
+};
 
-    return output.join('');
-  },
-
-  separate: function(branches, separations) {
-    var ret = branches.split('');
-    for(var i=0; i < separations.length; i++) {
-      if(separations[i] !== ' ' && ret[i] !== ' ') {
-        ret[i] = 'X';
-      }
-    }
-    return ret.join('');
+Branches.prototype.next = function() {
+  var output = [];
+  for(var i=0; i < this.val.length + 1; i++) {
+    output[i] = ' ';
   }
+
+  for(i=0; i < this.val.length; i++) {
+    switch(this.val[i]) {
+      case '<':
+        output[i] = output[i] == ' ' ? '<' : ' ';
+        break;
+      case '>':
+        output[i + 1] = '>';
+        break;
+      case 'X':
+        output[i] = output[i] == ' ' ? '<' : ' ';
+        output[i + 1] = '>';
+        break;
+    }
+  }
+
+  return new Branches(output);
+};
+
+Branches.prototype.separate = function(separations) {
+  var ret = this.val.slice(0);
+  for(var i=0; i < separations.length; i++) {
+    if(separations[i] !== ' ' && ret[i] !== ' ') {
+      ret[i] = 'X';
+    }
+  }
+  return new Branches(ret);
 };
--- test/test_branches.js
+++ test/test_branches.js
@@ -1,41 +1,41 @@
 module('Branches.next');
 test('blank yields blank', function() {
-  equal(Branches.next('  '), '   ');
+  equal(new Branches('  ').next().toString(), '   ');
 });
 test('left yields moving left', function() {
-  equal(Branches.next('<'), '< ');
+  equal(new Branches('<').next().toString(), '< ');
 });
 test('right yields moving right', function() {
-  equal(Branches.next('>'), ' >');
+  equal(new Branches('>').next().toString(), ' >');
 });
 test('X yields both sides moving opposite directions', function() {
-  equal(Branches.next('X'), '<>');
+  equal(new Branches('X').next().toString(), '<>');
 });
 test('parallel structures', function() {
-  equal(Branches.next('<<'), '<< ');
-  equal(Branches.next('<X'), '<<>');
-  equal(Branches.next('X>'), '<>>');
-  equal(Branches.next('>>'), ' >>');
+  equal(new Branches('<<').next().toString(), '<< ');
+  equal(new Branches('<X').next().toString(), '<<>');
+  equal(new Branches('X>').next().toString(), '<>>');
+  equal(new Branches('>>').next().toString(), ' >>');
 });
 test('left/right collision cancels out', function() {
-  equal(Branches.next('><'), '   ');
+  equal(new Branches('><').next().toString(), '   ');
 });
 test('X collision cancels out middle', function() {
-  equal(Branches.next('XX'), '< >');
+  equal(new Branches('XX').next().toString(), '< >');
 });
 test('X/left/right collisions', function() {
-  equal(Branches.next('X<'), '<  ');
-  equal(Branches.next('>X'), '  >');
-  equal(Branches.next('>X<'), '    ');
+  equal(new Branches('X<').next().toString(), '<  ');
+  equal(new Branches('>X').next().toString(), '  >');
+  equal(new Branches('>X<').next().toString(), '    ');
 });
 
 module('Branches.separate');
 test('no branches yields blank', function() {
-  equal(Branches.separate(' ', '.'), ' ');
+  equal(new Branches(' ').separate('.').toString(), ' ');
 });
 test('no separations yields original', function() {
-  equal(Branches.separate('<', ' '), '<');
+  equal(new Branches('<').separate(' ').toString(), '<');
 });
 test('separations yields X', function() {
-  equal(Branches.separate('<<>', '. .'), 'X<X');
+  equal(new Branches('<<>').separate('. .').toString(), 'X<X');
 });
