4e3f485b177ab6d4001d7dd08c99cc984d7f824f
fengb <benjamin.feng@gmail.com>
2012-10-28 19:24:20 -0500

    Converted Branches to be simple array with additional methods for easier [] access

--- src/branches.js
+++ src/branches.js
@@ -1,45 +1,54 @@
-function Branches(val) {
-  this.val = val;
-}
+var Branches = function(){
+  var instanceMethods = {
+    toString: function() {
+      return this.join('');
+    },
 
-Branches.fromString = function(str) {
-  return new Branches(str.split(''));
-};
+    next: function() {
+      var output = [];
+      for(var i=0; i < this.length + 1; i++) {
+        output[i] = ' ';
+      }
 
-Branches.prototype.toString = function() {
-  return this.val.join('');
-};
+      for(i=0; i < this.length; i++) {
+        switch(this[i]) {
+          case '<':
+            output[i] = output[i] == ' ' ? '<' : ' ';
+            break;
+          case '>':
+            output[i + 1] = '>';
+            break;
+          case 'X':
+            output[i] = output[i] == ' ' ? '<' : ' ';
+            output[i + 1] = '>';
+            break;
+        }
+      }
 
-Branches.prototype.next = function() {
-  var output = [];
-  for(var i=0; i < this.val.length + 1; i++) {
-    output[i] = ' ';
-  }
+      return Branches.init(output);
+    },
 
-  for(i=0; i < this.val.length; i++) {
-    switch(this.val[i]) {
-      case '<':
-        output[i] = output[i] == ' ' ? '<' : ' ';
-        break;
-      case '>':
-        output[i + 1] = '>';
-        break;
-      case 'X':
-        output[i] = output[i] == ' ' ? '<' : ' ';
-        output[i + 1] = '>';
-        break;
+    separate: function(separations) {
+      var ret = this.slice(0);
+      for(var i=0; i < separations.length; i++) {
+        if(separations[i] !== ' ' && ret[i] !== ' ') {
+          ret[i] = 'X';
+        }
+      }
+      return Branches.init(ret);
     }
-  }
+  };
 
-  return new Branches(output);
-};
+  return {
+    init: function(array) {
+      for(var method in instanceMethods) {
+        array[method] = instanceMethods[method];
+      }
+      return array;
+    },
 
-Branches.prototype.separate = function(separations) {
-  var ret = this.val.slice(0);
-  for(var i=0; i < separations.length; i++) {
-    if(separations[i] !== ' ' && ret[i] !== ' ') {
-      ret[i] = 'X';
+    fromString: function(str) {
+      return Branches.init(str.split(''));
     }
-  }
-  return new Branches(ret);
-};
+  };
+}();
