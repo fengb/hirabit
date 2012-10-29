c0c7d9cc922e1222783af0cab27bf754e2c47936
fengb <benjamin.feng@gmail.com>
2012-10-28 22:03:23 -0500

    Refactored into specific namespace for better QUnit support

--- src/branches.js
+++ src/branches.js
@@ -1,88 +1,93 @@
-var BranchRow = function(){
-  var instanceMethods = {
-    toString: function() {
-      return this.join('');
-    },
+var Branches = function(module) {
+  module.Row = function(){
+    var instanceMethods = {
+      toString: function() {
+        return this.join('');
+      },
 
-    next: function() {
-      var output = [];
-      for(var i=0; i < this.length + 1; i++) {
-        output[i] = ' ';
-      }
-
-      for(i=0; i < this.length; i++) {
-        switch(this[i]) {
-          case '<':
-            output[i] = output[i] == ' ' ? '<' : ' ';
-            break;
-          case '>':
-            output[i + 1] = '>';
-            break;
-          case 'X':
-            output[i] = output[i] == ' ' ? '<' : ' ';
-            output[i + 1] = '>';
-            break;
+      next: function() {
+        var output = [];
+        for(var i=0; i < this.length + 1; i++) {
+          output[i] = ' ';
         }
-      }
 
-      return BranchRow.init(output);
-    },
-
-    separate: function(separationRow) {
-      var ret = this.slice(0);
-      for(var i=0; i < separationRow.length; i++) {
-        if(separationRow[i] !== ' ' && ret[i] !== ' ') {
-          ret[i] = 'X';
+        for(i=0; i < this.length; i++) {
+          switch(this[i]) {
+            case '<':
+              output[i] = output[i] == ' ' ? '<' : ' ';
+              break;
+            case '>':
+              output[i + 1] = '>';
+              break;
+            case 'X':
+              output[i] = output[i] == ' ' ? '<' : ' ';
+              output[i + 1] = '>';
+              break;
+          }
         }
+
+        return module.Row.init(output);
+      },
+
+      separate: function(separationRow) {
+        var ret = this.slice(0);
+        for(var i=0; i < separationRow.length; i++) {
+          if(separationRow[i] !== ' ' && ret[i] !== ' ') {
+            ret[i] = 'X';
+          }
+        }
+        return module.Row.init(ret);
       }
-      return BranchRow.init(ret);
-    }
-  };
+    };
 
-  return {
-    init: function(array) {
-      for(var method in instanceMethods) {
-        array[method] = instanceMethods[method];
+    return {
+      init: function(array) {
+        for(var method in instanceMethods) {
+          array[method] = instanceMethods[method];
+        }
+        return array;
+      },
+
+      fromString: function(str) {
+        return module.Row.init(str.split(''));
       }
-      return array;
-    },
+    };
+  }();
 
-    fromString: function(str) {
-      return BranchRow.init(str.split(''));
+  module.ui = function() {
+    var separationRows = [];
+    for(var i = 0; i < 8; i++) {
+      separationRows[i] = Array(i + 2).join(' ').split('');
     }
-  };
-}();
 
 
-var separationRows = [];
-for(var i = 0; i < 8; i++) {
-  separationRows[i] = Array(i + 2).join(' ').split('');
-}
-
-
-var $rows = [];
-$.each(separationRows, function(i, separationRow) {
-  var $separationRow = $('<div />').appendTo('body');
-  $rows.push([]);
-  $.each(separationRow, function(j, separation) {
-    var $separation = $('<span>.</span>').appendTo($separationRow);
-    $rows[i].push($separation);
-    $separation.click(function() {
-      /* Cannot be separation because we mutate the row. */
-      separationRow[j] = separationRow[j] === ' ' ? '.' : ' ';
-      $separation.toggleClass('active', separationRow[j] !== ' ');
+    var $rows = [];
+    $.each(separationRows, function(i, separationRow) {
+      var $separationRow = $('<div />').appendTo('body');
+      $rows.push([]);
+      $.each(separationRow, function(j, separation) {
+        var $separation = $('<span>.</span>').appendTo($separationRow);
+        $rows[i].push($separation);
+        $separation.click(function() {
+          /* Cannot be separation because we mutate the row. */
+          separationRow[j] = separationRow[j] === ' ' ? '.' : ' ';
+          $separation.toggleClass('active', separationRow[j] !== ' ');
+        });
+      });
     });
-  });
-});
 
 
-$('<button>Run!</button>').appendTo('body').click(function() {
-  var branchRow = BranchRow.fromString('X');
-  for(var i = 0; i < separationRows.length; i++) {
-    branchRow = branchRow.separate(separationRows[i]);
-    for(var j = 0; j < branchRow.length; j++) {
-      $rows[i][j].html(branchRow[j].replace(' ', '&nbsp;'));
-    }
-    branchRow = branchRow.next();
-  }
-});
+    $('<button>Run!</button>').appendTo('body').click(function() {
+      var branchRow = module.Row.fromString('X');
+      for(var i = 0; i < separationRows.length; i++) {
+        branchRow = branchRow.separate(separationRows[i]);
+        for(var j = 0; j < branchRow.length; j++) {
+          $rows[i][j].html(branchRow[j].replace(' ', '&nbsp;'));
+        }
+        branchRow = branchRow.next();
+      }
+    });
+  };
+
+  return module;
+}({});
--- test/test_branches.js
+++ test/test_branches.js
@@ -1,41 +1,41 @@
-module('BranchRow.next');
+module('Branches.Row.next');
 test('blank yields blank', function() {
-  equal(BranchRow.fromString('  ').next().toString(), '   ');
+  equal(Branches.Row.fromString('  ').next().toString(), '   ');
 });
 test('left yields moving left', function() {
-  equal(BranchRow.fromString('<').next().toString(), '< ');
+  equal(Branches.Row.fromString('<').next().toString(), '< ');
 });
 test('right yields moving right', function() {
-  equal(BranchRow.fromString('>').next().toString(), ' >');
+  equal(Branches.Row.fromString('>').next().toString(), ' >');
 });
 test('X yields both sides moving opposite directions', function() {
-  equal(BranchRow.fromString('X').next().toString(), '<>');
+  equal(Branches.Row.fromString('X').next().toString(), '<>');
 });
 test('parallel structures', function() {
-  equal(BranchRow.fromString('<<').next().toString(), '<< ');
-  equal(BranchRow.fromString('<X').next().toString(), '<<>');
-  equal(BranchRow.fromString('X>').next().toString(), '<>>');
-  equal(BranchRow.fromString('>>').next().toString(), ' >>');
+  equal(Branches.Row.fromString('<<').next().toString(), '<< ');
+  equal(Branches.Row.fromString('<X').next().toString(), '<<>');
+  equal(Branches.Row.fromString('X>').next().toString(), '<>>');
+  equal(Branches.Row.fromString('>>').next().toString(), ' >>');
 });
 test('left/right collision cancels out', function() {
-  equal(BranchRow.fromString('><').next().toString(), '   ');
+  equal(Branches.Row.fromString('><').next().toString(), '   ');
 });
 test('X collision cancels out middle', function() {
-  equal(BranchRow.fromString('XX').next().toString(), '< >');
+  equal(Branches.Row.fromString('XX').next().toString(), '< >');
 });
 test('X/left/right collisions', function() {
-  equal(BranchRow.fromString('X<').next().toString(), '<  ');
-  equal(BranchRow.fromString('>X').next().toString(), '  >');
-  equal(BranchRow.fromString('>X<').next().toString(), '    ');
+  equal(Branches.Row.fromString('X<').next().toString(), '<  ');
+  equal(Branches.Row.fromString('>X').next().toString(), '  >');
+  equal(Branches.Row.fromString('>X<').next().toString(), '    ');
 });
 
-module('BranchRow.separate');
+module('Branches.Row.separate');
 test('no branches yields blank', function() {
-  equal(BranchRow.fromString(' ').separate('.').toString(), ' ');
+  equal(Branches.Row.fromString(' ').separate('.').toString(), ' ');
 });
 test('no separations yields original', function() {
-  equal(BranchRow.fromString('<').separate(' ').toString(), '<');
+  equal(Branches.Row.fromString('<').separate(' ').toString(), '<');
 });
 test('separations yields X', function() {
-  equal(BranchRow.fromString('<<>').separate('. .').toString(), 'X<X');
+  equal(Branches.Row.fromString('<<>').separate('. .').toString(), 'X<X');
 });
