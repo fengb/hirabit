61ad4ffad2a0c524ce1235a875a9cb4b8b5e9a93
fengb <benjamin.feng@gmail.com>
2012-11-03 21:07:42 -0500

    Refactored test comparison

--- test/Row/equivalentTo.js
+++ test/Row/equivalentTo.js
@@ -1,8 +1,28 @@
 module('Branches.Row:equivalentTo()');
 
+function equivalent(row1, row2) {
+  ok(row1.equivalentTo(row2), "'" + row1 + "' not equivalent to '" +
+                                    row2 + "' but should be");
+  ok(row2.equivalentTo(row1), "'" + row2 + "' not equivalent to '" +
+                                    row1 + "' but should be");
+}
+
+function notEquivalent(row1, row2) {
+  ok(!row1.equivalentTo(row2), "'" + row1 + "' equivalent to '" +
+                                     row2 + "' but shouldn't be");
+  ok(!row2.equivalentTo(row1), "'" + row2 + "' equivalent to '" +
+                                     row1 + "' but shouldn't be");
+}
+
 test('equivalent to self', function() {
   var row = Branches.Row.fromString(' *<X>* ');
-  ok(row.equivalentTo(row));
+  equivalent(row, row);
+});
+
+test('equivalent to same data but different instance', function() {
+  var row1 = Branches.Row.fromString(' *<X>* ');
+  var row2 = Branches.Row.fromString(' *<X>* ');
+  equivalent(row1, row2);
 });
 
 test('content differences are not equivalent', function() {
@@ -11,10 +31,7 @@ test('content differences are not equivalent', function() {
     for(var j = i + 1; j < chars.length; j++) {
       var row1 = Branches.Row.fromString(chars[i]);
       var row2 = Branches.Row.fromString(chars[j]);
-      ok(!row1.equivalentTo(row2), "'" + chars[i] + "' equivalent to '" +
-                                         chars[j] + "' but shouldn't be");
-      ok(!row2.equivalentTo(row1), "'" + chars[j] + "' equivalent to '" +
-                                         chars[i] + "' but shouldn't be");
+      notEquivalent(row1, row2);
     }
   }
 });
@@ -22,12 +39,5 @@ test('content differences are not equivalent', function() {
 test('different lengths are not equivalent', function() {
   var row1 = Branches.Row.fromString(' ');
   var row2 = Branches.Row.fromString('  ');
-  ok(!row1.equivalentTo(row2));
-});
-
-test('equivalent to same data but different instance', function() {
-  var row1 = Branches.Row.fromString(' *<X>* ');
-  var row2 = Branches.Row.fromString(' *<X>* ');
-  ok(row1.equivalentTo(row2));
-  ok(row2.equivalentTo(row1));
+  notEquivalent(row1, row2);
 });
