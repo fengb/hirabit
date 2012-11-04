1c2150a3dbe8ff67e234cf4bf3d83f93e02dba0c
fengb <benjamin.feng@gmail.com>
2012-11-04 00:29:18 -0500

    Refactored test

--- test/Row/equivalentTo.js
+++ test/Row/equivalentTo.js
@@ -1,13 +1,17 @@
 module('Branches.Row:equivalentTo()', {
   setup: function() {
-    this.equivalent = function(row1, row2) {
+    this.equivalent = function(rowString1, rowString2) {
+      var row1 = Branches.Row.fromString(rowString1);
+      var row2 = Branches.Row.fromString(rowString2);
       ok(row1.equivalentTo(row2), "'" + row1 + "' not equivalent to '" +
                                         row2 + "' but should be");
       ok(row2.equivalentTo(row1), "'" + row2 + "' not equivalent to '" +
                                         row1 + "' but should be");
     };
 
-    this.notEquivalent = function(row1, row2) {
+    this.notEquivalent = function(rowString1, rowString2) {
+      var row1 = Branches.Row.fromString(rowString1);
+      var row2 = Branches.Row.fromString(rowString2);
       ok(!row1.equivalentTo(row2), "'" + row1 + "' equivalent to '" +
                                          row2 + "' but shouldn't be");
       ok(!row2.equivalentTo(row1), "'" + row2 + "' equivalent to '" +
@@ -16,36 +20,23 @@ module('Branches.Row:equivalentTo()', {
   }
 });
 
-test('equivalent to self', function() {
-  var row = Branches.Row.fromString(' *<X>* ');
-  this.equivalent(row, row);
-});
-
-test('equivalent to same data but different instance', function() {
-  var row1 = Branches.Row.fromString(' *<X>* ');
-  var row2 = Branches.Row.fromString(' *<X>* ');
-  this.equivalent(row1, row2);
+test('equivalent to same data', function() {
+  this.equivalent(' *<X>* ', ' *<X>* ');
 });
 
 test('blank equivalent to merge', function() {
-  var row1 = Branches.Row.fromString(' ');
-  var row2 = Branches.Row.fromString('*');
-  this.equivalent(row1, row2);
+  this.equivalent(' ', '*');
 });
 
 test('content differences are not equivalent', function() {
   var chars = ' <>X';
   for(var i = 0; i < chars.length; i++) {
     for(var j = i + 1; j < chars.length; j++) {
-      var row1 = Branches.Row.fromString(chars[i]);
-      var row2 = Branches.Row.fromString(chars[j]);
-      this.notEquivalent(row1, row2);
+      this.notEquivalent(chars[i], chars[j]);
     }
   }
 });
 
 test('different lengths are not equivalent', function() {
-  var row1 = Branches.Row.fromString(' ');
-  var row2 = Branches.Row.fromString('  ');
-  this.notEquivalent(row1, row2);
+  this.notEquivalent(' ', '  ');
 });
